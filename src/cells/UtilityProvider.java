d935a70ccacc37ae5d0b304cafcdb55599b1e013
ARDA AKYOL <ardaakyol9311@gmail.com>
2026-06-04 16:34:53 +0300

    UtilityProvider

--- src/cells/UtilityProvider.java
+++ src/cells/UtilityProvider.java
@@ -1,4 +1,26 @@
 package cells;
 
-public class UtilityProvider {
+public abstract class UtilityProvider extends Cell {
+
+    protected String utilityType;
+    protected int capacity;
+
+    public UtilityProvider(int row, int col, char symbol, String utilityType) {
+        super(row, col, symbol);
+        this.utilityType = utilityType;
+        this.capacity = 100;
+    }
+
+    public String getUtilityType() {
+        return utilityType;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public boolean isConnectable() {
+        return true;
+    }
 }
