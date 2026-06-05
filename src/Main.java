eb9a832bddffc5f46b9b5a5433a62426814849d2
omerduaci9-stack <omerduaci9@gmail.com>
2026-06-05 18:14:40 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Usage: java Main <mapfile> <ticks>");
+            return;
+        }
+
+        try {
+            String mapFile = args[0];
+            int ticks = Integer.parseInt(args[1]);
+
+            CityMap cityMap = new CityMap(mapFile);
+            GameEngine engine = new GameEngine(cityMap);
+
+            engine.run(ticks);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Tick count must be an integer.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
